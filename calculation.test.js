d220f3cefc9107cfa219ac815e6c711e7f43d847
junwoo45 <sinyoorang@gmail.com>
2020-06-23 15:48:30 +0900

    test: 필요할 것으로 예상되는 sum객체 테스트

--- calculation.test.js
+++ calculation.test.js
@@ -23,6 +23,40 @@ class Money {
   getCurrency() {
     return this.currency;
   }
+
+  plus(add) {
+    return new Sum(this.amount, add.amount)
+  }
+
+  reduce(amount, to) {
+    // 이렇게 새로운 인스턴스를 생성하는게 맞나....
+    return new Money(amount, to);
+  }
+}
+
+class Bank {
+  reduce(source, to) {
+    if(source instanceof Money) {
+      // return source;
+      return source.reduce(source.amount, to);
+    }
+
+    const sum = source;
+
+    return sum.reduce(to);
+  }
+}
+
+class Sum {
+  constructor(augend, addend) {
+    this.augend = augend;
+    this.addend = addend;
+  }
+
+  reduce(to) {
+    const amount = this.augend + this.addend;
+    return new Money(amount, to);
+  }
 }
 
 // 테스트 케이스
@@ -73,3 +107,41 @@ test('Currency test', () => {
   expect('USD').toBe(money.dollar(1).getCurrency());
   expect('CHF').toBe(money.franc(1).getCurrency());
 });
+
+test('SIMPLE Addition TEST', () => {
+  const money = new Money();
+  const five = money.dollar(5);
+  // const sum = five.plus(five);
+  const sum = new Sum(money.dollar(5).amount, money.dollar(5).amount);
+
+  const bank = new Bank();
+  const reduced = bank.reduce(sum, 'USD');
+
+  expect(money.dollar(10).amount).toBe(reduced.amount);
+});
+
+test('Plus Returns Sum', () => {
+  const money = new Money();
+  const five = money.dollar(5);
+  const sum = five.plus(five);
+
+  expect(five.amount).toBe(sum.augend);
+  expect(five.amount).toBe(sum.addend);
+});
+
+test('Reduce Sum test', () => {
+  const money = new Money();
+  const sum = new Sum(money.dollar(3).amount, money.dollar(4).amount);
+
+  const bank = new Bank();
+  const reduced = bank.reduce(sum, 'USD');
+
+  expect(money.dollar(7).amount).toBe(reduced.amount);
+});
+
+test('Reduce Money test', () => {
+  const bank = new Bank();
+  const result = bank.reduce(new Money().dollar(1), 'USD');
+
+  expect(new Money().dollar(1).amount).toBe(result.amount);
+})
