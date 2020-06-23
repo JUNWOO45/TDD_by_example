770d195b263fd821633d6287b72ae96a532ff237
junwoo45 <sinyoorang@gmail.com>
2020-06-23 11:43:36 +0900

    feat: 상위 클래스로 메소드들 이동

--- calculation.test.js
+++ calculation.test.js
@@ -1,52 +1,56 @@
-class Dollar {
+class Money {
   constructor(amount) {
     this.amount = amount;
+  }
+
+  equals(instance) {
+    return this.amount === instance.amount;
+  }
+}
+
+class Dollar extends Money{
+  constructor(amount) {
+    super(amount);
   }
 
   times(multiplier) {
     return new Dollar(this.amount * multiplier);
   }
-
-  equals(instance) {
-    return this.amount === instance.amount;
-  }
 }
 
-class Franc {
+class Franc extends Money{
   constructor(amount) {
-    this.amount = amount;
+    super(amount);
   }
 
   times(multiplier) {
     return new Franc(this.amount * multiplier);
   }
-
-  equals(instance) {
-    return this.amount === instance.amount;
-  }
 }
 
-
 // 테스트 케이스
-
 test('Franc class test', () => {
   const five = new Franc(5);
-
-  expect(new Franc(10).amount).toBe(five.times(2).amount);
-  expect(new Franc(15).amount).toBe(five.times(3).amount);
+  expect(five.times(2).amount).toBe(new Franc(10).amount);
+  expect(five.times(3).amount).toBe(new Franc(15).amount);
 })
 
-test('multiply test', () => {
+test('Dollay class test', () => {
   const five = new Dollar(5);
   expect(five.times(2).amount).toBe((new Dollar(10)).amount);
   expect(five.times(5).amount).toBe((new Dollar(25)).amount);
 });
 
 test('test equality', () => {
-  const five = new Dollar(5);
-  const ex1 = five.equals(new Dollar(5))
-  const ex2 = five.equals(new Dollar(6))
+  const ex1 = new Dollar(5).equals(new Dollar(5));
+  const ex2 = new Dollar(5).equals(new Dollar(6));
 
   expect(ex1).toBeTruthy();
   expect(ex2).toBeFalsy();
+
+  const ex3 = new Franc(7).equals(new Franc(7));
+  const ex4 = new Franc(7).equals(new Franc(8));
+
+  expect(ex3).toBeTruthy();
+  expect(ex4).toBeFalsy();
 })
